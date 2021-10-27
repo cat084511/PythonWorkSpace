b9b4cf19d982608d0c2be81c89aee77fd2ac28dc
cat084511 <cat084511@gmail.com>
2021-10-27 12:15:17 +0900

    main

--- 1027/08/if_2.py
+++ 1027/08/if_2.py
@@ -1,25 +1,11 @@
-import re
-import unicodedata
-
-def is_japanese(string):
-    for ch in string:
-        name = unicodedata.name(ch) 
-        if "CJK UNIFIED" in name \
-        or "HIRAGANA" in name \
-        or "KATAKANA" in name \
-            return True
-    return False
 
 while 1:
     e = input()
     if e=="":
         break
-    elif is_japanese(e):
-        print("jap")
     elif e.isdigit():
         print("数字")
-    elif re.match('^[a-zA-Z]+$',e):
-    #e.isalpha()は日本語もTrueになる
+    elif e.isalpha()
         print("アルファベット")
     elif e.isalnum():
         print("数字とアルファベット")
