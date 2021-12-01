1fb7ddb0d13c4e25ec5ce28ad111a5e32e969258
cat084511 <cat084511@gmail.com>
2021-12-01 12:37:15 +0900

    main

--- 1201/choice.py
+++ 1201/choice.py
@@ -6,4 +6,4 @@ def choice_func(number):
     return odd if int(number)%2 else even
 # main
 while (num:=input("数字を入力してください。（0：終了）"))!="0":
-    print("入力が正しくありません") if num.isdecimal() else choice_func(num)()
+    choice_func(num)() if num.isdecimal() else print("入力が正しくありません") 
