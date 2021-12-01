02b2cc84e06835df728930bc278d239b0fff3d56
cat084511 <cat084511@gmail.com>
2021-12-01 10:36:07 +0900

    main

--- 1124/func_3.py
+++ 1124/func_3.py
@@ -2,18 +2,20 @@ def do_anything(*args):
     print("受け取った値：",args)
     if len(args)==0:
         print("やること無いので暇です")
+
     elif len(args)==1:
         if type(args[0]) is str or str(args[0]).isdigit():
             print(args[0]*2)
         else:print("難しくて無理です")
+
     elif len(args)==2:
         if not(isinstance(args[0],(str,int)) or isinstance(args[1],(str,int))):
             print("無茶言わないで！")
         elif type(args[0])==type(args[1]):
             print(args[0]+args[1])
         else:print("できません〜")
-    else:print("無理です…")
 
+    else:print("無理です…")
 
 # main
 do_anything()
